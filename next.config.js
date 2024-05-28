d4a5063ca505223c69855442fa33054750edb534
Edgar Yael Anderson Flores <91216296+Ander42069@users.noreply.github.com>
2024-05-28 09:54:39 -0600

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["lh3.googleusercontent.com"],
-      output: "export",
-    },
-  };
-  
-  module.exports = nextConfig;
+  output: "export",
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
+
+module.exports = nextConfig;
